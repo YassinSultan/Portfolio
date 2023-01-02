2fd6a35ed9dd8620dc5b76fd0cd00556ea5c71d3
yaseen-sultan18 <eng.yaseen.sultan@gmail.com>
2023-01-02 19:50:25 +0200

    Uploading Project files

---
+++ js/myscript.js
@@ -0,0 +1,24 @@
+AOS.init();
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 30,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    initialSlide: 2,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+ 
